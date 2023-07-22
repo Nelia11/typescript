0d13ee8085d8cc6812e9bb05c4d523ea1471abd4
Nelia11 <levit.nelia@icloud.com>
2023-07-22 16:29:10 +0200

    add ListTemplate

--- public/app.js
+++ public/app.js
@@ -1,23 +1,14 @@
 import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
-let docOne;
-let docTwo;
-docOne = new Invoice("yoshi", "web work", 259);
-docTwo = new Payment("mario", "plumbing work", 200);
-let docs = [];
-docs.push(docOne);
-docs.push(docTwo);
-const invOne = new Invoice("mario", "work on the mario website", 250);
-const invTwo = new Invoice("luigi", "work on the mario website", 300);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach(inv => console.log(inv.amount, inv.client, inv.format()));
 const form = document.querySelector(".new-item-form");
 const type = document.getElementById("type");
 const tofrom = document.getElementById("tofrom");
 const details = document.getElementById("details");
 const amount = document.getElementById("amount");
+// list template 
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     let doc;
@@ -27,5 +18,5 @@ form.addEventListener("submit", (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, "end");
 });
